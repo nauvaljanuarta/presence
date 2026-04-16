99d16ecf0b417daf965b742bdc69e34e62cf8b6a
ndajatoh01 <nauvalaryanto2005@gmail.com>
2026-04-16 11:29:11 +0700

    update modul 3

--- GAS Presence App/config.js
+++ GAS Presence App/config.js
@@ -1,6 +1,3 @@
 const DEFAULT_API_URL = "https://script.google.com/macros/s/AKfycbwRWRN2Nn3vvDzWNO8IYinc449oZcnyCCC3NJmzqTX0gSnOyFpEzo4Giap3nYEwg39M4g/exec";
-<<<<<<< HEAD
+
 const API_URL = localStorage.getItem("API_URL") || DEFAULT_API_URL;
-=======
-const API_URL = localStorage.getItem("API_URL") || DEFAULT_API_URL;
->>>>>>> d424c3fd8490002c7758143e154100a2fcb8671d
